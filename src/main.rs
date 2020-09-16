99bcc7bb41e96c7df09fad06068ac1eea11cce4c
Caleb Trachte <cetrachte@hotmail.com>
2020-09-16 12:50:38 -0500

    add new route

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,18 @@
 
 #[macro_use] extern crate rocket;
 
+use rocket::http::RawStr;
+
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/hello/<name>")]
+fn hello(name: &RawStr) -> String {
+    format!("Hello, {}!", name.as_str())
+}
+
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite().mount("/", routes![index, hello]).launch();
 }
