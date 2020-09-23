89a87f728e2b0b1e0d26d55a0f56710519676943
Caleb Trachte <cetrachte@hotmail.com>
2020-09-23 14:08:49 -0500

    add new routes for counting visits

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,9 @@
 use rocket::http::RawStr;
 use rocket::response::NamedFile;
 use rocket_contrib::serve::StaticFiles;
+use rocket::State;
+use std::sync::atomic::{AtomicUsize, Ordering};
+use rocket::response::content;
 
 #[get("/")]
 fn index() -> Option<NamedFile> {
@@ -21,9 +24,25 @@ pub fn hello() -> &'static str {
     "Hello, outside world!"
 }
 
+struct HitCount(AtomicUsize);
+
+#[get("/visitors")]
+fn visitors(hit_count: State<HitCount>) -> content::Html<String> {
+    hit_count.0.fetch_add(1, Ordering::Relaxed);
+    let msg = "Your visit has been recorded!";
+    let count = format!("Visits: {}", count(hit_count));
+    content::Html(format!("{}<br /><br />{}", msg, count))
+}
+
+#[get("/count")]
+fn count(hit_count: State<HitCount>) -> String {
+    hit_count.0.load(Ordering::Relaxed).to_string()
+}
+
 fn main() {
     rocket::ignite()
-    .mount("/", routes![index, hello, hello_name])
+    .mount("/", routes![index, hello, hello_name, visitors, count])
     .mount("/static", StaticFiles::from("static"))
+    .manage(HitCount(AtomicUsize::new(0)))
     .launch();
 }
