697591a62b12b9c2c0321b30778c805605fd9378
Caleb Trachte <cetrachte@hotmail.com>
2020-10-07 13:29:08 -0500

    updated deprecated syntax

--- src/main.rs
+++ src/main.rs
@@ -68,7 +68,7 @@ fn wow_helper(
     _: &Handlebars,
     _: &Context,
     _: &mut RenderContext,
-    out: &mut Output
+    out: &dyn Output
 ) -> HelperResult {
     if let Some(param) = h.param(0) {
         out.write("<b><i>")?;
